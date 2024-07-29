4e8aa9a2b6e6fdb6ece234491213eda2ce9a5209
NotoriousFleaD <adko2404@gmail.com>
2024-07-29 13:50:13 +0200

    Pridanie vypocet casu ku hre

--- ZapisHodin.java
+++ ZapisHodin.java
@@ -8,6 +8,7 @@ public class ZapisHodin{
   private String datum;
   private ArrayList hra, casStravenyHry; 
   private LocalTime zaciatokCasu2, koniecCasu2, stravenyCas;
+  private String[][] hry_a_cas;
   
   public ZapisHodin(){
     Scanner sc = new Scanner(System.in);
@@ -20,7 +21,7 @@ public class ZapisHodin{
     do {
       System.out.print("V aky cas si prisiel: ");
       zaciatokCasu = sc.nextLine();
-  
+      
       if(zaciatokCasu.length() != 5){
         zaciatokCasu = opravaCasu(zaciatokCasu);
         
@@ -36,7 +37,7 @@ public class ZapisHodin{
     do {
       System.out.print("V aky cas si odisiel: ");
       koniecCasu = sc.nextLine();
-  
+      
       if(koniecCasu.length() != 5){
         koniecCasu = opravaCasu(koniecCasu);
         
@@ -49,12 +50,50 @@ public class ZapisHodin{
     
     stravenyCas = vypocetStravenehoCasu(zaciatokCasu2, koniecCasu2);
     
-    System.out.print("Zadaj nazov hry: ");
-    hra.add(sc.nextLine());
+    LocalTime zostavajuciCas = stravenyCas;
     
     System.out.print("Zadaj nazov hry: ");
     hra.add(sc.nextLine());
-     
+    
+    System.out.print("Zadaj kolko casu si odohral v " + hra.get(0) + ": ");
+    String casHra = sc.nextLine();
+    
+    LocalTime premennaCas = premenaNaLocalTime(zostavajuciCas, casHra);
+    
+    hry_a_cas = new String[1][2];
+    //tu som skoncil, debata nema reci 
+    hry_a_cas[0][0] = "" + hra.get(0);
+    hry_a_cas[0][1] = "" + String.valueOf(premennaCas);
+    
+    //    zostavajuciCas = zostavajuciCas.of(zostavajuciCas.getHour() - premennaCas.getHour(), zostavajuciCas.getMinute() - premennaCas.getMinute());
+    zostavajuciCas = zostavajuciCas.minusHours(premennaCas.getHour());
+    zostavajuciCas = zostavajuciCas.minusMinutes(premennaCas.getMinute());
+    
+    //    switch(hra.size()){
+    //      case 1:
+    //        hry_a_cas = new String[1][2];
+    //        
+    //        hry_a_cas[0][0] = "" + hra.get(0);
+    //        hry_a_cas[0][0] = String.valueOf(stravenyCas);
+    //        break;
+    //        
+    //      default:
+    //        do{
+    //          hry_a_cas = new String[hra.size()][2];
+    //          
+    //          // Inicializacia hier do pola
+    //          for(int i = 0; i < hra.size(); i++){
+    //            hry_a_cas[i][0] = "" + hra.get(i);  //<<< nechaapem
+    //          }
+    //          
+    //        
+    //        }while(zostavajuciCas.getHour() != 0 && zostavajuciCas.getMinute() != 0);
+    //    }
+    
+    
+    
+    
+    
   }
   
   public ZapisHodin(String datum, LocalTime zaciatokCasu, LocalTime koniecCasu, ArrayList hra, ArrayList casStravenyHry){
@@ -84,7 +123,7 @@ public class ZapisHodin{
     char[] pole = cas.toCharArray();
     char[] charCas = new char[2];
     int intCas = 0;
-  
+    
     //Hodiny
     for(int i = 0; i < 2; i++){
       charCas[i] = pole[i];
@@ -161,10 +200,61 @@ public class ZapisHodin{
     LocalDate curDate = LocalDate.now();
     return formatter.format(curDate);
   }
+  
+//  public void vypisHierSCasom(){
+//    
+//  }
+
+  public LocalTime premenaNaLocalTime(LocalTime cas, String hodnotaCasu){
+    char[] pole1 = new char[2]; 
+    char[] pole2 = hodnotaCasu.toCharArray();
+    int i = 0;
+    
+    pole1 = vynulovaniePola(pole1, 2);
+    for (i = 0; pole2[i] != 'h'; i++) {
+      char buffer = pole1[0];
+      pole1[0] = pole1[1];
+      pole1[1] = pole1[0];
+ 
+      pole1[1] = pole2[i];
+    }
+
+    int c = Integer.parseInt(String.valueOf(pole1));
+    cas = cas.minusHours(c);
+    
+    
+    System.out.println("pred>" + cas.getMinute());
+    
+    
+    i+=2; // aby sa prekocila medzera
+    pole1 = vynulovaniePola(pole1, 2);
+    for (int j = i; pole2[j] != 'm'; j++ ) {
+      char buffer = pole1[0];
+      pole1[0] = pole1[1];
+      pole1[1] = pole1[0];
+ 
+      pole1[1] = pole2[j];
+      System.out.println(pole1);
+    }
+    cas = cas.minusMinutes(Integer.valueOf(String.valueOf(pole1)));
+    System.out.println("po>" + cas.getMinute());
+
+    System.out.println("KONIEC");
+    return cas;
+  }
+
+  public char[] vynulovaniePola(char[] pole, int velkost){
+    for(int i = 0; i < velkost; i++){
+      pole[i] = '0';
+    }
+    
+    return pole;
+  }
 
   
+  
   public String toString(){
-    return "Datum: " + datum + "\nZaciatok prace: " + zaciatokCasu2 + "\nKoniec prace: " + koniecCasu2 + "\nHry: " + getHry() + "\nCelkovy straveny cas v praci: " + stravenyCas + " --> " +stravenyCas.getHour() + "h " + stravenyCas.getMinute() + "min" + "\nID: " + super.toString();
+    return "Datum: " + datum + "\nZaciatok prace: " + zaciatokCasu2 + "\nKoniec prace: " + koniecCasu2 + "\nHry: " + getHry() + "\nCelkovy straveny cas v praci: " + stravenyCas + " --> " + stravenyCas.getHour() + "h " + stravenyCas.getMinute() + "min" + "\nID: " + super.toString();
   }
   
   public void info(){
